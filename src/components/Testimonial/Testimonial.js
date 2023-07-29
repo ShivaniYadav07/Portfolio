7a8de8669622c4dd0c88c3a0c3c3125a9747e7d2
Shivani Yadav <shivaniyadavguru@gmail.com>
2023-07-30 00:50:14 +0530

    Changes in Testimonial

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,76 +1,113 @@
 import React from "react";
 import StarParticles from "../Particle";
-import { Container, Row,Col, Card } from "react-bootstrap";
-
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import downloadImage from "../../Assets/picture11.png";
+import "./Testimonial.css";
+import { CgWebsite } from "react-icons/cg";
 
 function Testimonial() {
-    return (
-        <section>
-            <Container fluid className="about-section">
-<StarParticles />
-<Container>
-<Row style={{ justifyContent: "center", padding: "10px" }}>
+  
+  return (
+    <section>
+      <Container fluid className="about-section">
+        <StarParticles />
+        <Container>
+          <Row style={{ justifyContent: "center", padding: "10px" }}>
+            <Col
+              md={7}
+              style={{
+                justifyContent: "center",
+                paddingTop: "30px",
+                paddingBottom: "50px",
+              }}
+            >
+              <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+              Feedback from<strong className="purple"> Valued Clients</strong>
+              </h1>
 
-<Col
-            md={7}
-            style={{
-              justifyContent: "center",
-              paddingTop: "30px",
-              paddingBottom: "50px",
-            }}
+              <blockquote className="blockquote mb-0">
+      <p style={{ fontSize: "1em", paddingBottom: "20px", textAlign: "justify" }}>
+      These testimonials showcase the <strong className="purple">experiences, successes, and positive</strong> outcomes they have encountered while collaborating<strong className="purple"> with me.</strong>
+              </p>
+              </blockquote>
+            </Col>
+          </Row>
+          <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="tcard">
+          <Card className="project-card-view d-flex justify-content-center align-items-center">
+      <Card.Img className="p-0 img-fluid w-50" variant="top" src={downloadImage} roundcircle="true"/>
+      <Card.Body className="text-center">
+        <Card.Title>Aasif Ansari, Dehradun</Card.Title>
+        <hr />
+        <Card.Subtitle>FullStack Developer</Card.Subtitle>
+        <hr />
+        <Card.Subtitle>Web UI Design and Backend</Card.Subtitle>
+        <hr />
+        <Card.Text>
+        "I've collaborated with many developers, but this one truly stands out.As a full stack developer myself, I was impressed by her expertise and dedication to producing high-quality work."
+        </Card.Text>
+        <div variant="primary" className="rating mt-3 mt-md-0 mb-4 mb-md-0 mb-lg-4 mb-xl-0"><i className="px-1 text-warning"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg></i><i className="px-1 text-warning"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg></i><i className="px-1 text-warning"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg></i><i className="px-1 text-warning"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg></i><i className="px-1 text-warning"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg></i></div>
+      </Card.Body>
+      <Button
+            variant="primary"
+            href="https://www.amaalagroup.com/"
+            target="_blank"
+            style={{ marginBottom: "10px" }}
           >
-    <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              What Client <strong className="purple">Says</strong>
-            </h1>
-
-    </Col>
-    </Row>
-    <Row>
-    <Card className="project-card-view" style={{ width: '22rem',marginRight: '22px'}}>
-    <Col xs={6} md={4}>
-        </Col>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text style={{ textAlign: "justify" }}>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
+            <CgWebsite /> &nbsp;
+            {"See Work"}
+          </Button>
     </Card>
-    <Card className="project-card-view" style={{ width: '22rem', marginRight: '22px' }}>
-    <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text style={{ textAlign: "justify" }}>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+</Col>
+          <Col md={4} className="tcard">
+          <Card className="project-card-view d-flex justify-content-center align-items-center">
+      <Card.Img className="p-0 img-fluid w-50" variant="top" src={downloadImage} roundcircle="true"/>
+      <Card.Body className="text-center">
+        <Card.Title>Krishna, Delhi</Card.Title>
+        <hr />
+        <Card.Subtitle>FullStack Developer</Card.Subtitle>
+        <hr />
+        <Card.Subtitle>User-friendly Web Frontend, Backend</Card.Subtitle>
+        <hr />
+        <Card.Text>
+        "From all sides she is demonstrated a deep understanding of our project's goals and requirements. Her expertise in web development is truly exceptional, and she has a keen eye for detail that reflects in the polished and user-friendly websites they create."
         </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <div variant="primary" className="rating mt-3 mt-md-0 mb-4 mb-md-0 mb-lg-4 mb-xl-0"><i className="px-1 text-warning"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg></i><i className="px-1 text-warning"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg></i><i className="px-1 text-warning"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg></i><i className="px-1 text-warning"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg></i><i className="px-1 text-warning"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg></i></div>
       </Card.Body>
+      <Button
+            variant="primary"
+            href="https://breezestone.shivaniyadav.online/"
+            target="_blank"
+            style={{ marginBottom: "10px" }}
+          >
+            <CgWebsite /> &nbsp;
+            {"See Work"}
+          </Button>
     </Card>
-    <Card className="project-card-view" style={{ width: '22rem', marginRight: '22px' }}>
-    <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text style={{ textAlign: "justify" }}>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+</Col>
+          {/* <Col md={4} className="tcard">
+          <Card className="project-card-view d-flex justify-content-center align-items-center">
+      <Card.Img className="p-0 img-fluid w-50" variant="top" src={downloadImage} roundcircle="true"/>
+      <Card.Body className="text-center">
+        <Card.Title></Card.Title>
+        <hr />
+        <Card.Subtitle>FullStack Developer</Card.Subtitle>
+        <hr />
+        <Card.Text>
+        "I've collaborated with many developers, but this one truly stands out.As a full stack developer myself, I was impressed by her expertise and dedication to producing high-quality work."
         </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <div variant="primary" className="rating mt-3 mt-md-0 mb-4 mb-md-0 mb-lg-4 mb-xl-0"><i className="px-1 text-warning"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg></i><i className="px-1 text-warning"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg></i><i className="px-1 text-warning"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg></i><i className="px-1 text-warning"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg></i><i className="px-1 text-warning"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg></i></div>
       </Card.Body>
+      
     </Card>
-    </Row>
-    </Container>
+</Col> */}
+          </Row>
 
-</Container>
-</section>
-    )};
+      </Container>
 
-    export default Testimonial;
+      </Container>
+    </section>
+  );
+}
+
+export default Testimonial;
